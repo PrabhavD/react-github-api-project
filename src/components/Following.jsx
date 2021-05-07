86c09bbac921a8872481749a800fb7cc8ee76864
Prabhav Desai <63824619+PrabhavD@users.noreply.github.com>
2021-05-06 18:13:02 -0600

    Update Following.jsx

--- src/components/Following.jsx
+++ src/components/Following.jsx
@@ -1,1 +1,52 @@
+import React, {Component} from 'react';
+import GithubUser from './GithubUser';
 
+class Following extends React.Component {
+    
+    state = {
+        following: []
+    }
+
+    fetchData(){
+        fetch(`https://api.github.com/users/${this.props.params.username}/following`)
+        .then(response => response.json())
+        .then(
+            following => {
+                // How can we use `this` inside a callback without binding it??
+                // Make sure you understand this fundamental difference with arrow functions!!!
+                this.setState({
+                    following: following
+                });
+            }
+        );
+    }
+    
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.username !== this.props.params.username) {
+            this.fetchData();
+        }
+    }
+
+    render() {
+        if (!this.state.following) {
+            return <div>LOADING FOLLOWING...</div>
+        }
+            
+        return (
+        <div className="following-page">
+            <h2>Following of {this.props.params.username}</h2>
+            <ul>
+                {this.state.following.map(
+                    (chosenFollowing, chosenKey) => <GithubUser user={chosenFollowing} key={chosenKey} />
+                )}
+            </ul>
+        </div>
+        );
+    }
+};
+
+export default Following;
