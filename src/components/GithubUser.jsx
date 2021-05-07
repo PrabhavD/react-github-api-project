31a03975814cad919267c13261bf968f93df7c04
Prabhav Desai <63824619+PrabhavD@users.noreply.github.com>
2021-05-06 18:13:41 -0600

    Update GithubUser.jsx

--- src/components/GithubUser.jsx
+++ src/components/GithubUser.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import {Link} from 'react-router';
 
+class GithubUser extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        var user = this.props.user;
+        return (
+            <Link to={`/user/${user.login}`}>
+             <img src={user.avatar_url}/>
+             <p>{user.login}</p>
+            </Link>
+        );
+    }
+};
+
+export default GithubUser;
