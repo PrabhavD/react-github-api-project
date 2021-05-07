41bfd117884a5910df269541e58972da4e3d3194
Prabhav Desai <63824619+PrabhavD@users.noreply.github.com>
2021-05-06 18:14:39 -0600

    Update GithubRepo.jsx

--- src/components/GithubRepo.jsx
+++ src/components/GithubRepo.jsx
@@ -1,1 +1,20 @@
+import React from 'react';
 
+
+class GithubRepo extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <a href={this.props.repo.url}>
+                <p>{this.props.repo.full_name}</p>
+            </a>
+        );
+    }
+};
+
+export default GithubRepo;
